e7fc82758d824256ee21d80175ae03e077cecd93
Artem Dybov <xivrussian@gmail.com>
2022-09-14 11:25:47 +0300

    Readme

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 export DJANGO_SETTINGS_MODULE='codeteamtask.settings'
-pip install -r dependencies.txt
+pip3 install -r dependencies.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 sh run.sh
